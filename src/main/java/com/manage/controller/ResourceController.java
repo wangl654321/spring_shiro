b62a924a64958cb2894599472a3c4d504f4d1dff
王路 <lu.wang@emaxcard.com>
2019-04-08 09:25:32 +0800

    优化

--- src/main/java/com/manage/controller/ResourceController.java
+++ src/main/java/com/manage/controller/ResourceController.java
@@ -16,10 +16,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-/**
- * @description：资源管理
- * @author：zhixuan.wang
- * @date：2015/10/1 14:51
+/***
+ *
+*
+* 描    述：资源管理
+*
+* 创 建 者： @author wl
+* 创建时间： 2019/4/8 9:04
+* 创建描述：
+*
+* 修 改 者：
+* 修改时间：
+* 修改描述：
+*
+* 审 核 者：
+* 审核时间：
+* 审核描述：
+*
  */
 @Controller
 @RequestMapping("/resource")
