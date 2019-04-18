1bf8eb77544a2def7a0fe636d9b799442ca2ba7e
王路 <lu.wang@emaxcard.com>
2019-04-18 09:40:49 +0800

    优化

--- src/main/java/com/manage/shiro/CustomerAuthenticationToken.java
+++ src/main/java/com/manage/shiro/CustomerAuthenticationToken.java
@@ -1,21 +1,19 @@
 package com.manage.shiro;
 
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /***
  *
  *
- * 描    述：
+ * 描    述：记住密码 remenberMe配置
  *
  * 创 建 者：@author wl
- * 创建时间：2019/4/179:08
+ * 创建时间：2019/4/179:52
  * 创建描述：
  *
- * 修 改 者：  
- * 修改时间： 
- * 修改描述： 
+ * 修 改 者：
+ * 修改时间：
+ * 修改描述：
  *
  * 审 核 者：
  * 审核时间：
@@ -24,15 +22,13 @@ import org.slf4j.LoggerFactory;
  */
 public class CustomerAuthenticationToken extends UsernamePasswordToken {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    private String captcha;
-
     /**
-     * 用来区分登录用户的渠道
+     * 记住密码
+     *
+     * @param username
+     * @param password
+     * @param rememberMe
      */
-    private String loginForm;
-
     public CustomerAuthenticationToken(String username, String password, boolean rememberMe) {
         super(username, password, rememberMe);
     }
