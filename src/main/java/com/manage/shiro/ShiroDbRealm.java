1e52f20cd2dd98433266820b7c16080019f967f4
王路 <lu.wang@emaxcard.com>
2019-04-22 09:43:35 +0800

    优化

--- src/main/java/com/manage/shiro/ShiroDbRealm.java
+++ src/main/java/com/manage/shiro/ShiroDbRealm.java
@@ -50,8 +50,7 @@ public class ShiroDbRealm extends AuthorizingRealm {
      * Shiro登录认证(原理：用户提交 用户名和密码  --- shiro 封装令牌 ---- realm 通过用户名将密码查询返回 ---- shiro 自动去比较查询出密码和用户输入密码是否一致---- 进行登陆控制 )
      */
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(
-            AuthenticationToken authcToken) throws AuthenticationException {
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
         LOGGER.info("Shiro开始登录认证");
         UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
         SysUser user = userService.findUserByLoginName(token.getUsername());
@@ -74,12 +73,10 @@ public class ShiroDbRealm extends AuthorizingRealm {
      * Shiro权限认证
      */
     @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(
-            PrincipalCollection principals) {
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
 
         ShiroUser shiroUser = (ShiroUser) principals.getPrimaryPrincipal();
         List<Long> roleList = shiroUser.roleList;
-
         Set<String> urlSet = Sets.newHashSet();
         for (Long roleId : roleList) {
             //查询角色对应的资源地址
